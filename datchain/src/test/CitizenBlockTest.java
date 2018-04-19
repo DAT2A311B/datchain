24ef6fc3ad399e1405073800d988b20fe0cacbe3
cogitantium <falke.cs@gmail.com>
2018-04-19 02:12:04 +0200

    added further CitizenBlock tests and structure, needs refactoring

--- datchain/src/test/CitizenBlockTest.java
+++ datchain/src/test/CitizenBlockTest.java
@@ -1,4 +1,37 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+
 public class CitizenBlockTest {
+    @Test
+    void computeHash() {
+    }
+
+    @Test
+    void getHash() {
+    }
+
+    @Test
+    void getPrevHash() {
+    }
+
+    @Test
+    void getTimestamp() {
+    }
+
+    @Test
+    void calcIndex() {
+    }
+
+    @Test
+    void getIndex() {
+    }
+
+    @Test
+    void getPubKey() {
+    }
+
+    @Test
+    void getIdentity() {
+    }
 }
--- datchain/src/test/DatchainTest.java
+++ datchain/src/test/DatchainTest.java
@@ -1,30 +1,44 @@
 package test;
 
-import main.CitizenBlock;
+import main.*;
 import org.junit.jupiter.api.Test;
 
 import static java.time.Instant.now;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-
-public class DatchainTest {
+class DatchainTest {
 
     @Test
-    public void testTimestampConstructor()  {
+    void testBlockDifferent() {
+        //mostly learning, but might be useful
+        CitizenBlock block01 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
+        CitizenBlock block02 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
+        assertFalse(block01.equals(block02));
+    }
+
+    @Test
+    void testBlockEqual() {
+        //mostly learning, but might be useful
+        CitizenBlock block01 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
+        assertEquals(block01, block01);
+    }
+
+    @Test
+    void testTimestampConstructor()  {
         CitizenBlock block01 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "0000");
         assertEquals(now().getEpochSecond(),block01.getTimestamp());
     }
 
     @Test
-    public void testNameCongruency() {
+    void testNameCongruency() {
         CitizenBlock block01 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
         assertEquals(block01.getIdentity(), "Citizen Name");
         //TODO Must also test validator info
     }
 
     @Test
-    public void testHashCongruency() {
+    void testHashCongruency() {
         //naive by testing two identical objects, could be done more advanced
         CitizenBlock block01 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
         CitizenBlock block02 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
