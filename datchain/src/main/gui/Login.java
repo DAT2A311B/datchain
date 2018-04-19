e33152fe94e7a55a05f715d33ea4517f341dd482
MikkelFilip <mikkelfilip1@gmail.com>
2018-04-19 12:49:11 +0200

    finished login page and made it un-resizeable

--- datchain/src/main/gui/Login.java
+++ datchain/src/main/gui/Login.java
@@ -10,25 +10,22 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Login {
-    public static void login(Stage primaryStage){
+    public static void login(Stage primaryStage) {
         var root = new VBox(10);
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(10, 10, 10, 10));
-        var scene = new Scene(root, 600, 400);
+        var scene = new Scene(root, 600, 150);
+        primaryStage.setResizable(false);
 
-        var ID_label = new Label("ID:");
+        var ID_label = new Label("Private-ID:");
         root.getChildren().add(ID_label);
 
-        var ID_text = new TextField("");
+        var ID_text = new TextField();
         root.getChildren().add(ID_text);
 
-        var Password_label = new Label("Password:");
-        root.getChildren().add(Password_label);
-
-        var Password_text = new TextField("");
-        root.getChildren().add(Password_text);
-
         var Login_button = new Button("Login");
+        Login_button.setMinWidth(80);
+        Login_button.setMinHeight(30);
         root.getChildren().add(Login_button);
 
         primaryStage.setScene(scene);
--- datchain/src/main/gui/wrapper.java
+++ datchain/src/main/gui/wrapper.java
@@ -1,5 +1,20 @@
 package main.gui;
 
-public class wrapper {
- lidt tekst
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class wrapper extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Datchain");
+        Login.login(primaryStage);
+    }
+
 }
+
+
