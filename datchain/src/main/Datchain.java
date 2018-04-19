f299e1e6e050a59c08ee815aa6b5d96013a21ada
cogitantium <falke.cs@gmail.com>
2018-04-19 02:27:17 +0200

    implemented an arraylist of blocks, a blockchain

--- datchain/src/main/Datchain.java
+++ datchain/src/main/Datchain.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Datchain {
 
     public static void main(String[] args) {
@@ -8,9 +11,16 @@ public class Datchain {
         CitizenBlock block02 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
         CitizenBlock block03 = new CitizenBlock("Validator","ValidatorPubkey", "Citizen Name", "CitizenPubKey", "9817324939382");
 
-        System.out.println(block01.getHash());
-        System.out.println(block02.getHash());
-        System.out.println(block03.getHash());
+        //TODO consider using linked lists for another dimension of blocks - beware
+        List<Block> chain01 = new ArrayList<>();
+
+        chain01.add(block01);
+        chain01.add(block02);
+        chain01.add(block03);
+
+        System.out.println("n blocks in chain01: " + chain01.size());
+        
+
     }
 
 }
