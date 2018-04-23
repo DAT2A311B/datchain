98e50f6a946f0561de4fcd9bd3f4c360cd1e5056
cogitantium <falke.cs@gmail.com>
2018-04-23 18:43:59 +0200

    added better inheritance and constructor to ValidatorBlock

--- datchain/src/main/ValidatorBlock.java
+++ datchain/src/main/ValidatorBlock.java
@@ -11,64 +11,24 @@ public class ValidatorBlock extends Block {
 
     //TODO validators should be authorised through RSA-signed challenge/hash from genesis authority
     private String genesisSignature;
-    private String validatorIdent;
-    private String validatorPubKey;
+    private String identity;
+    private String identityPublicKey;
     private String hash;
     private String prevHash;
     private long timestamp;
 
-    public ValidatorBlock(String genesisSignature, String validatorIdent, String validatorPubKey, String hash, String prevHash, long timestamp) {
+    public ValidatorBlock(String genesisSignature, String identity, String identityPublicKey, String hash, String prevHash, long timestamp) {
         this.genesisSignature = genesisSignature;
-        this.validatorIdent = validatorIdent;
-        this.validatorPubKey = validatorPubKey;
+        this.identity = identity;
+        this.identityPublicKey = identityPublicKey;
         this.prevHash = prevHash;
         this.timestamp = now().getEpochSecond();
-        this.hash = computeHash();
+        //is using getters better than using already assigned parameters?
+        this.hash = computeHash(getIdentity() + getIdentityPublicKey() +
+        getPrevHash() + getTimestamp() + getGenesisSignature());
     }
 
-
-    public String computeHash() {
-        //TODO should grab and implement chain-index
-        String hashInput = validatorIdent + validatorPubKey + prevHash + timestamp + genesisSignature;
-
-        MessageDigest messageDigest = null;
-
-        try {
-            messageDigest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        //TODO might be redundant or not proper error-handling
-        assert messageDigest != null;
-
-        //get hashInput as bytes in UTF-8 and update messageDigest
-        messageDigest.update(hashInput.getBytes(StandardCharsets.UTF_8));
-        //create digest byte-array with resulting hash and encode digest as hex-string
-        return String.format("%064x", new BigInteger(1, messageDigest.digest()));
-    }
-
-    @Override
-    public String getHash() {
-        return this.hash;
-    }
-
-    @Override
-    public String getPrevHash() {
-        return this.prevHash;
-    }
-
-    @Override
-    public long getTimestamp() {
-        return this.timestamp;
-    }
-
-    @Override
-    public String getIdentityPublicKey() {
-        return validatorPubKey;
-    }
-
-    @Override
-    public String getIdentity() {
-        return validatorIdent;
+    private String getGenesisSignature() {
+        return this.genesisSignature;
     }
 }
