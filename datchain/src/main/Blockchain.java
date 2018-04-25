1a5af0393ca353303a29fa0aaf2293e1d16d4f36
cogitantium <falke.cs@gmail.com>
2018-04-25 19:29:45 +0200

    added work on validation and block-datatype check for addValidatedBlock method

--- datchain/src/main/Blockchain.java
+++ datchain/src/main/Blockchain.java
@@ -4,10 +4,39 @@ import java.util.ArrayList;
 
 public class Blockchain extends ArrayList<Block> implements Chain {
 
+    public boolean addValidatedBlock(Block block, Block validator) {
+
+        //check if block to be added is of GenesisBlock-type and if chainsize is 0
+        if (GenesisBlock.class.isAssignableFrom(block.getClass()) && this.size() == 0) {
+            System.out.println("Block is of GenesisBlock-type");
+
+        //check if block to be added is of ValidatorBlock-type and if chainsize is greater than 0
+        } else if (ValidatorBlock.class.isAssignableFrom(block.getClass()) && this.size() > 0) {
+            System.out.println("Block is of ValidatorBlock-type and chain has at least one block");
+
+        //check if block to be added is of CitizenBlock-type and if chainsize is greater than 0
+        } else if (CitizenBlock.class.isAssignableFrom(block.getClass()) && this.size() > 0) {
+            System.out.println("Block is of CitizenBlock-type and chain has at least one block");
+
+        //if none match, block is not recognized and a fatal error has occurred
+        } else {
+            throw new RuntimeException("ERROR: Block supplied does not match any types known!");
+        }
+
+        //check if supplied validator block exists on chain, needs RSA-challenge as well
+        if (this.contains(validator)) {
+            System.out.println("Is validator");
+            this.add(block);
+        } else {
+            throw new RuntimeException("Validator is not on chain, cannot guarantee authority");
+        }
+        return true;
+    }
+
+    //might not be necessary
     @Override
     public boolean add(Block block) {
-        //Should implement a validator check, if not genesis
-        //if (validator)
+
         try {
             super.add(block);
             return true;
@@ -15,31 +44,6 @@ public class Blockchain extends ArrayList<Block> implements Chain {
             System.out.println("Caught IllegalArgumentException: " + e.getMessage());
             return false;
         }
-    }
-
-    public Blockchain getChain() {
-        return this;
-    }
-
-    //ArrayList doesn't implement a .last() method, thus we implement one ourselves
-    public Block getHead() {
-        Block head;
-        if (this.size() > 0) {
-            head = this.get(this.size() - 1);
-        } else {
-            throw new RuntimeException("No blocks added, can't get head");
-        }
-        return head;
-    }
-
-    public Block getBlock(int index) {
-        Block block;
-        try {
-            block = get(index);
-        } catch (IndexOutOfBoundsException e) {
-            throw new IndexOutOfBoundsException("Index out of bounds, can't get block");
-        }
-        return block;
     }
 
     public boolean validateChain() {
@@ -67,4 +71,30 @@ public class Blockchain extends ArrayList<Block> implements Chain {
         //if no congruency errors are found, chain is valid
         return true;
     }
+
+    //ArrayList doesn't implement a .last() method, thus we implement one ourselves
+    public Block getHead() {
+        Block head;
+        if (this.size() > 0) {
+            head = this.get(this.size() - 1);
+        } else {
+            throw new RuntimeException("No blocks added, can't get head");
+        }
+        return head;
+    }
+
+    public Block getBlock(int index) {
+        Block block;
+        try {
+            block = get(index);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("Index out of bounds, can't get block");
+        }
+        return block;
+    }
+
+    public Blockchain getChain() {
+        return this;
+    }
+
 }
--- datchain/src/main/Datchain.java
+++ datchain/src/main/Datchain.java
@@ -6,11 +6,11 @@ public class Datchain {
 
         GenesisBlock genesis01 = new GenesisBlock("Genesis", "GenesisPublicKey", "0000");
 
-        ValidatorBlock validator01 = new ValidatorBlock("Validator", "ValidatorPublicKey", "ValidatorPrevHash", "GenesisSignature");
-        ValidatorBlock validator02 = new ValidatorBlock("Validator", "ValidatorPublicKey", "ValidatorPrevHash", "GenesisSignature");
-        ValidatorBlock validator03 = new ValidatorBlock("Validator", "ValidatorPublicKey", "ValidatorPrevHash", "GenesisSignature");
+        ValidatorBlock validator01 = new ValidatorBlock("Validator", "ValidatorPublicKey", genesis01.getHash(), "GenesisSignature");
+        ValidatorBlock validator02 = new ValidatorBlock("Validator", "ValidatorPublicKey", validator01.getHash(), "GenesisSignature");
+        ValidatorBlock validator03 = new ValidatorBlock("Validator", "ValidatorPublicKey", validator02.getHash(), "GenesisSignature");
 
-        CitizenBlock citizen01 = new CitizenBlock("Citizen Name1", "CitizenPublicKey", "4321j6hj7234kj5234n", validator01.getIdentity(), validator01.getIdentityPublicKey());
+        CitizenBlock citizen01 = new CitizenBlock("Citizen Name1", "CitizenPublicKey", validator03.getHash(), validator01.getIdentity(), validator01.getIdentityPublicKey());
         CitizenBlock citizen02 = new CitizenBlock("Citizen Name2", "CitizenPublicKey", citizen01.getHash(), validator02.getIdentity(), validator02.getIdentityPublicKey());
         CitizenBlock citizen03 = new CitizenBlock("Citizen Name3", "CitizenPublicKey", citizen02.getHash(), validator03.getIdentity(), validator03.getIdentityPublicKey());
 
@@ -19,16 +19,10 @@ public class Datchain {
         chain02.add(validator01);
         chain02.add(validator02);
         chain02.add(validator03);
-        
+
         chain02.add(citizen01);
         chain02.add(citizen02);
         chain02.add(citizen03);
-
-        System.out.println("n blocks in chain02: " + chain02.size());
-
-        System.out.println("chain02 contains citizen01? " + chain02.contains(citizen01));
-
-        System.out.println("index of citizen03: " + chain02.indexOf(citizen03));
 
         System.out.println("chain02 validated: " + chain02.validateChain());
 
